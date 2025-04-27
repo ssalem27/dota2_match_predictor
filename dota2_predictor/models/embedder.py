ba9b02146e5cfc685255b46924fdf22104181a76
ssalem27 <safialhasi@gmail.com>
2025-04-26 21:07:09 -0400

    revert hero to static embedding

--- dota2_predictor/models/embedder.py
+++ dota2_predictor/models/embedder.py
@@ -3,25 +3,21 @@ import torch
 
 
 class HeroEmbeddings(nn.Module):
-    def __init__(self,embedding_dim,projection_dim,output_size):
+    def __init__(self,embedding_dim):
         super().__init__()
         self.primary_attr = nn.Embedding(4,embedding_dim)
         self.attack_type = nn.Embedding(2,embedding_dim)
-        self.roles_embed = nn.Linear(8,embedding_dim)
-        self.projection = nn.Sequential(nn.Linear(22,projection_dim),nn.ReLU(),nn.Linear(projection_dim,projection_dim//2))
-        total_size = 3*embedding_dim+(projection_dim//2)
-        self.combine = nn.Sequential(nn.Linear(total_size,output_size),nn.ReLU(),nn.LayerNorm(output_size))
-
+        self.roles_embed = nn.EmbeddingBag(8,embedding_dim)
+        self.projection = nn.Linear(22,16)
 
     def forward(self,p_attrs,a_types,role_i,float_stats):
         pa_embed = self.primary_attr(p_attrs)
         attack_embed = self.attack_type(a_types)
-        role_embed = self.roles_embed(role_i)
-        stats_embed = self.projection(float_stats)
+        role_embed = self.roles_embed(role_i.unsqueeze(0))
+        stats_embed = self.projection(float_stats.unsqueeze(0))
         hero = torch.cat([pa_embed,attack_embed,role_embed,stats_embed],dim=1)
-        hero = self.combine(hero)
-        return hero
-
+        return hero.squeeze(0)
+    
 class TeamComp(nn.Module):
     def __init__(self,embedding_dim,num_attention,output_dim):
         super().__init__()
